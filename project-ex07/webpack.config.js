169e008a9bad69e62c7f83c3b5e1f6a9d659288f
unknown <swj03322@naver.com>
2020-04-28 14:02:32 +0900

    commit

--- project-ex07/webpack.config.js
+++ project-ex07/webpack.config.js
@@ -2,18 +2,25 @@ const path = require('path');
 
 module.exports = {
     entry: path.resolve('src/index.js'),
-    output: {
+    output:{
         path: path.resolve('public'),
         filename: 'bundle.js'
     },
-    devServer: {
+    module:{
+        rules:[{
+            test : /\.js$/,
+            exclude: /node_modules/,
+            loader: 'babel-loader'
+        }]
+    },
+     devServer:{
         contentBase: path.resolve('public'),
-        host: '0.0.0.0',
+        host:'0.0.0.0',
         port: 9999,
         inline: true,
         liveReload: true,
         hot: false,
         compress: true,
         historyApiFallback: true
-    }    
+    }
 }
